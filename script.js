42033964cb924142027e5ff30f6d5621be7fc323
sharull9 <sharullmusicplaylist@gmail.com>
2022-09-21 17:20:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
+function url(){
+    const main = document.querySelector('#url')
+    const input = main.children[0];
+    const output = main.children[1].children[0];
+    const button = main.children[1].children[1];
 
-const input = document.querySelector('#value');
-const output = document.querySelector('#output');
-const button = document.querySelector('#copyToClipboard');
+    console.log(input)
+    input.addEventListener('focus', ()=>{
+        input.select();
+    })
+    
+    input.addEventListener('input', ()=> {
+        let str = input.value;
+        let lowerCase = str.toLowerCase().replaceAll('.', '')
+        let removeComma = lowerCase.replaceAll("'", '-')
+        let finalValue = removeComma.split(" ").join("-");
+        output.innerText = finalValue;
+    })
+    
+    button.addEventListener('click', ()=> {
+        let copyText = output.innerText;
+        // copyText.select();
+        navigator.clipboard.writeText(copyText);
+        console.log(copyText)
+    })
+}
 
+url()
 
-input.addEventListener('focus', ()=>{
-    input.select();
-})
+function upper(){
+    const main = document.querySelector('#upper')
+    const input = main.children[0];
+    const output = main.children[1].children[0];
+    const button = main.children[1].children[1];
 
-input.addEventListener('input', ()=> {
-    let str = input.value;
-    let lowerCase = str.toLowerCase().replaceAll('.', '')
-    let removeComma = lowerCase.replaceAll("'", '-')
-    let finalValue = removeComma.split(" ").join("-");
-    output.innerText = finalValue;
-})
+    console.log(input)
+    input.addEventListener('focus', ()=>{
+        input.select();
+    })
+    
+    input.addEventListener('input', ()=> {
+        let str = input.value;
+        let upperCase = str.toUpperCase()
+        output.innerText = upperCase;
+    })
+    
+    button.addEventListener('click', ()=> {
+        let copyText = output.innerText;
+        // copyText.select();
+        navigator.clipboard.writeText(copyText);
+        console.log(copyText)
+    })
+}
 
-button.addEventListener('click', ()=> {
-    let copyText = output.innerText;
-    // copyText.select();
-    navigator.clipboard.writeText(copyText);
-    console.log(copyText)
-})
+upper()
