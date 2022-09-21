fa8fa518f59632bd0edb199aec00b6314f9588ad
sharull9 <sharullmusicplaylist@gmail.com>
2022-09-21 12:26:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const input = document.querySelector('#value');
 const output = document.querySelector('#output');
 const button = document.querySelector('#button');
 
+const button2 = document.querySelector('#copyToClipboard');
+
 button.addEventListener('click', ()=> {
     let str = input.value;
     let lowerCase = str.toLowerCase().replaceAll('.', '')
     let removeComma = lowerCase.replaceAll("'", '-')
     let finalValue = removeComma.split(" ").join("-");
     output.innerText = finalValue;
+})
+
+button2.addEventListener('click', ()=> {
+    let copyText = output.innerText;
+    // copyText.select();
+    navigator.clipboard.writeText(copyText);
+    console.log(copyText)
 })
